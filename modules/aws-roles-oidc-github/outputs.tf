1dc106f5641fb12ba757a4216fc57f018df71031
Joachim Hill-Grannec <joachim.hill@gmail.com>
2026-04-24 16:10:30 -0400

    feat: v1.1 validations and iam_role_names output (#17)

--- modules/aws-roles-oidc-github/outputs.tf
+++ modules/aws-roles-oidc-github/outputs.tf
@@ -2,3 +2,8 @@ output "iam_role_arn" {
   description = "Role that will be assumed by GitHub Action"
   value       = aws_iam_role.github_ci.arn
 }
+
+output "iam_role_name" {
+  description = "Name of the IAM role created for this GitHub Actions entry."
+  value       = aws_iam_role.github_ci.name
+}
